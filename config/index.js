79ffc989605f9da2d98e10a170a269304073a02d
guoqingbo <qingbo.guo@zhiyoubao.com>
2019-06-03 10:52:51 +0800

    修改bug添加订单查询页

--- config/index.js
+++ config/index.js
@@ -54,7 +54,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // assetsPublicPath: './',
+    assetsPublicPath: '/',//不采用绝对路径，刷新时报错
 
     /**
      * Source Maps
