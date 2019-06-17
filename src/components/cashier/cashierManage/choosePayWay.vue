27f43c3b01a444057dee5d309f7d4d2bcf615047
guoqingbo <qingbo.guo@zhiyoubao.com>
2019-06-17 18:23:17 +0800

    添加收银台

--- src/components/cashier/cashierManage/choosePayWay.vue
+++ src/components/cashier/cashierManage/choosePayWay.vue
@@ -235,11 +235,13 @@
           console.log(res)
           this.loading = false
           if(res.xml.result_code && res.xml.result_code._text == 'SUCCESS'){
+            // 如果
             if(res.xml.redirect){
               window.location.href = res.xml.code_url
               // window.open(res.xml.code_url, '_blank');
               return
             }
+            // 二维码弹框
             this.modal = true;
             this.qrcodeUrl = res.xml.code_url._text;
           }else{
