ebe1c09ad59283a2ab8027bbb77f5d913af2d6f7
guoqingbo <qingbo.guo@zhiyoubao.com>
2019-08-19 14:22:00 +0800

    农行山上传证书修改

--- src/components/cashier/cashierManage/callWxPay.vue
+++ src/components/cashier/cashierManage/callWxPay.vue
@@ -10,7 +10,7 @@
     },
     methods: {
       wxPay() {
-        let {appid,timestamp,noncestr,packagestr,signtype,paysign} = this.$route.query
+        let {appid,timestamp,noncestr,packagestr,signtype,paysign,redirectUrl} = this.$route.query
         let payParams = {
           appId:appid,
           timeStamp:timestamp,
@@ -27,9 +27,9 @@
               if (res.err_msg == "get_brand_wcpay_request:ok") {
                 // 使用以上方式判断前端返回,微信团队郑重提示：
                 //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-
+                document.location.href = redirectUrl;
               } else {
-
+                alert("支付成功，跳转失败")
               }
             });
         }
