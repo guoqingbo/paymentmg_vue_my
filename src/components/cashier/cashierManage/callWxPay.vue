1d56496976ca6f6fd5a6fc27b114047b78b9f15d
guoqingbo <qingbo.guo@zhiyoubao.com>
2019-10-15 10:11:04 +0800

    支付成功回掉打印

--- src/components/cashier/cashierManage/callWxPay.vue
+++ src/components/cashier/cashierManage/callWxPay.vue
@@ -10,6 +10,7 @@
     },
     methods: {
       wxPay() {
+        alert(window.location.href)
         let {appid,timestamp,noncestr,packagestr,signtype,paysign,redirectUrl,orderSource,redirectUrlError} = this.$route.query
         let payParams = {
           appId:appid,
@@ -27,9 +28,9 @@
               if (res.err_msg == "get_brand_wcpay_request:ok") {
                 // 使用以上方式判断前端返回,微信团队郑重提示：
                 //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-                document.location.href = redirectUrl+"&orderSource="+orderSource;
+                window.location.href = redirectUrl+"&orderSource="+orderSource;
               } else {
-                document.location.href = redirectUrlError+"&orderSource="+orderSource;
+                window.location.href = redirectUrlError+"&orderSource="+orderSource;
                 // alert("支付成功，跳转失败")
               }
             });
