4e52c185e917eab487d02edc7441cefdfc1f2a80
guoqingbo <qingbo.guo@zhiyoubao.com>
2019-10-15 10:35:58 +0800

    支付成功回掉打印

--- src/components/cashier/cashierManage/callWxPay.vue
+++ src/components/cashier/cashierManage/callWxPay.vue
@@ -28,9 +28,37 @@
               if (res.err_msg == "get_brand_wcpay_request:ok") {
                 // 使用以上方式判断前端返回,微信团队郑重提示：
                 //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-                window.location.href = redirectUrl+"&orderSource="+orderSource;
+                // 回掉地址如果有参数
+                let href = ''
+                if(/\?/.test(redirectUrl)){
+                  href = redirectUrl+"&orderSource="+orderSource;
+                }else{
+                  href = redirectUrl+"?orderSource="+orderSource;
+                }
+                window.location.href = href;
+                // 跳转错误页面
+                // this.$router.push({
+                //   path: "/cashier/cashierManage/payError",
+                //   query:{
+                //     message:res.message
+                //   }
+                // });
               } else {
-                window.location.href = redirectUrlError+"&orderSource="+orderSource;
+                // window.location.href = redirectUrlError+"&orderSource="+orderSource;
+                let href = ''
+                if(/\?/.test(redirectUrlError)){
+                  href = redirectUrlError+"&orderSource="+orderSource;
+                }else{
+                  href = redirectUrlError+"?orderSource="+orderSource;
+                }
+                window.location.href = href;
+                // 跳转错误页面
+                // this.$router.push({
+                //   path: "/cashier/cashierManage/payError",
+                //   query:{
+                //     message:res.message
+                //   }
+                // });
                 // alert("支付成功，跳转失败")
               }
             });
